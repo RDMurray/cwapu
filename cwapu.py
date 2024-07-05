8f7fc5debf0d65a47196a42772381b684d441b8f
GabrieleBattaglia <iz4apu@libero.it>
2024-07-05 14:06:50 +0200

    Added groups to RX Exercise.

--- cwapu.py
+++ cwapu.py
@@ -1,16 +1,28 @@
-# CWAPU2 - Utility per il CW, di Gabry, IZ4APU
-# Data cncepimento 21/12/2022.
+# CWAPU - Utility per il CW, di Gabry, IZ4APU
+# Data concepimento 21/12/2022.
 # GitHub publishing on july 2nd, 2024.
 
-import sys, random, pickle
+import sys, random, pickle, string
 import datetime as dt
-from string import ascii_uppercase, digits
-from GBUtils import key, dgt
+from GBUtils import key, dgt, menu
 from cwzator import *
 from time import localtime as lt
 
 #constants
-VERS="0.9.9.5 Beta, february 2023"
+VERS="1.0-Beta, february 2023"
+MNMAIN={
+	"c":"Counting results",
+	"t":"Transmitting exercise",
+	"r":"Receiving exercise",
+	"m":"shows Menu",
+	"q":"To quit this app"}
+MNRX={
+	"1":"Call-like",
+	"2":"Groups"}
+MNRXKIND={
+										"1":"Letters only",
+										"2":"Numbers only",
+										"3":"Letters and Numbers"}
 MDL={'a0a':4,
 					'a0aa':6,
 					'a0aaa':15,
@@ -25,15 +37,22 @@ MDL={'a0a':4,
 					'a00aaa':4}
 	
 #quif
+def GeneratingGroup(kind, length):
+	if kind == "1":
+		return ''.join(random.choice(string.ascii_letters) for _ in range(length))
+	elif kind == "2":
+		return ''.join(random.choice(string.digits) for _ in range(length))
+	elif kind == "3":
+		return ''.join(random.choice(string.ascii_letters + string.digits) for _ in range(length))
 def Mkdqrz(c):
 	#Sub of Txing
 	q=''
 	c=c[0]
 	for j in str(c):
 		if j.isdigit():
-			q+=random.choice(digits)
+			q+=random.choice(string.digits)
 		else:
-			q+=random.choice(ascii_uppercase)
+			q+=random.choice(string.ascii_uppercase)
 	return q
 
 def Txing():
@@ -119,15 +138,6 @@ def Count():
 	print("Bye for now, back to main menu.")
 	return
 
-def VMenu():
-	# Shows menu
-	print("\nMenu:")
-	print(" - ( C ) - Counting results;")
-	print(" - ( T ) - Transmitting exercise;")
-	print(" - ( R ) - Receiving exercise;")
-	print(" - ( M ) - shows Menu;")
-	print(" - (ESC) - Exit this app.")
-	return
 
 def Rxing():
 	# receiving exercise
@@ -136,19 +146,29 @@ def Rxing():
 		f=open("CWapu_Rxing.pkl", "rb")
 		wpm, totalcalls, sessions, totalget, totalwrong, totaltime = pickle.load(f)
 		f.close()
-		print(f"I got your data from disk, so:\nYour actual WPM is {wpm} and you did {sessions-1} sessions.\nI sent to you {totalcalls} total calls-like, and you got {totalget} of them, while {totalwrong} were missed\nYour overall time receiving calls-like is {str(totaltime)[:-5]}.")
+		print(f"I got your data from disk, so:\nYour actual WPM is {wpm} and you did {sessions-1} sessions.\nI sent to you {totalcalls} total calls-like or groups, and you got {totalget} of them, while {totalwrong} were missed\nYour overall time receiving calls-like is {str(totaltime)[:-5]}.")
 	except IOError:
 		print("Ups, this is your first class, probably. So I'm creating the record.")
 		wpm, totalcalls, sessions, totalget, totalwrong, totaltime = 22, 0, 1, 0, 0, dt.datetime.now()-dt.datetime.now()
 	calls, callsget, callswrong, callsrepeated, minwpm, maxwpm, repeatedflag = 0, [], [], 0, 100, 14, False
-	print("Now, careful. Type the call-like you hear.\nGiving an empty line (or adding a ?) will gift you a second listen to the call-like.\n\tTo stop, just type a '.' (fullstop) followed by enter.\nENJOY. \tPress any key when you're ready to start.")
+	print("Now select which exercise do you want to take:")
+	call_or_groups=menu(d=MNRX,show=True,keyslist=True,ntf="Please, just 1 or 2")
+	if call_or_groups == "2":
+		kind=menu(d=MNRXKIND,show=True,keyslist=True,ntf="1 2 or 3, please")
+		length=dgt(prompt="Give me the length of the group in between 1 and 7: ",kind="i",imin=1,imax=7)
+		kindstring="Group"
+	else: kindstring="Call-like"
+	print(f"Now, careful. Type the {kindstring} you hear.\nGiving an empty line (or adding a ?) will gift you a second listen to the {kindstring}.\n\tTo stop, just type a '.' (fullstop) followed by enter.\nENJOY. \tPress any key when you're ready to start.")
 	attesa=key()
 	print(f"Let's begin session {sessions}!")
 	starttime=dt.datetime.now()
 	while True:
-		c=random.choices(list(MDL.keys()), weights=MDL.values(), k=1)
-		qrz=Mkdqrz(c)
-		pitch=random.randint(450, 750)
+		if call_or_groups == "1":
+			c=random.choices(list(MDL.keys()), weights=MDL.values(), k=1)
+			qrz=Mkdqrz(c)
+		else:
+			qrz=GeneratingGroup(kind=kind, length=length)
+		pitch=random.randint(350, 850)
 		prompt=f"S{sessions}-#{calls} - WPM{wpm} - +{len(callsget)}/-{len(callswrong)} - > "
 		CWzator(msg=qrz, wpm=wpm, pitch=pitch)
 		guess=dgt(prompt=prompt, kind="s", smin=0, smax=6)
@@ -172,16 +192,16 @@ def Rxing():
 	exerctime=dt.datetime.now()-starttime
 	print("It's over! Now let me check what we've got.")
 	if calls>14:
-		print(f"In this session #{sessions}, I sent {calls} calls-like to you and you got {len(callsget)} of them: {len(callsget)*100/calls:.1f}%")
+		print(f"In this session #{sessions}, I sent {calls} {kindstring} to you and you got {len(callsget)} of them: {len(callsget)*100/calls:.1f}%")
 		print(f"\t{len(callsget)-callsrepeated} of these has been taken at the first shot: {(len(callsget)-callsrepeated)*100/len(callsget):.1f}%")
-		print(f"\twhile {callsrepeated} calls-like with repetition: {callsrepeated*100/len(callsget):.1f}%.")
+		print(f"\twhile {callsrepeated} {kindstring} with repetition: {callsrepeated*100/len(callsget):.1f}%.")
 		print(f"You ran with a minimum speed of {minwpm} up to {maxwpm}: range of {maxwpm-minwpm} WPM.")
 		f=open("CWapu_Diary.txt", "a")
 		print("Report saved on CW_Diary.txt")
 		f.write(f"Receiving exercise #{sessions} performed on {str(lt()[0])}/{str(lt()[1])}/{str(lt()[2])} at {str(lt()[3])}, {str(lt()[4])} minutes:\n")
-		f.write(f"In this session #{sessions}, I sent {calls} calls-like to you and you got {len(callsget)} of them: {len(callsget)*100/calls:.1f}%\n")
+		f.write(f"In this session #{sessions}, I sent {calls} {kindstring} to you and you got {len(callsget)} of them: {len(callsget)*100/calls:.1f}%\n")
 		f.write(f"\t{len(callsget)-callsrepeated} of these has been taken at the first shot: {(len(callsget)-callsrepeated)*100/len(callsget):.1f}%\n")
-		f.write(f"\twhile {callsrepeated} calls-like with repetition: {callsrepeated*100/len(callsget):.1f}%.\n")
+		f.write(f"\twhile {callsrepeated} {kindstring} with repetition: {callsrepeated*100/len(callsget):.1f}%.\n")
 		f.write(f"You ran with a minimum speed of {minwpm} up to {maxwpm}: range of {maxwpm-minwpm} WPM.\n")
 		nota=dgt(prompt="Note on this exercise: ", kind="s", smin=0, smax=512)
 		if nota != "":
@@ -189,7 +209,7 @@ def Rxing():
 		else:
 			f.write(f"Note: empty\n***\n")
 		f.close()
-	else: print("You received too few calls-like to generate a consistant statistics.")
+	else: print(f"You received too few {kindstring} to generate a consistant statistics.")
 	totalcalls+=calls
 	totalget+=len(callsget)
 	totalwrong+=len(callswrong)
@@ -203,15 +223,13 @@ def Rxing():
 
 #main
 print(f"CWAPU - VERSION: {VERS} BY GABE - IZ4APU.\n----UTILITIES FOR YOUR CW----")
-VMenu()
 while True:
-	k=key("\nC.T.R.M.ESC.? ", 600)
-	k=k.lower()
+	k=menu(d=MNMAIN,show=True,keyslist=True,ntf="It's not a command!")
 	if k=="c": Count()
 	elif k=="t": Txing()
 	elif k=="r": Rxing()
-	elif k=="m": VMenu()
-	elif ord(k)==27: break
+	elif k=="m": menu(d=MNMAIN,show_only=True)
+	elif k=="q": break
 	else:
 		print(f"- - {k} is unknown.")
 		VMenu()
